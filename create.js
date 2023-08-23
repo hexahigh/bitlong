d781bd6d755b757b84c7104a4d76bcd13b642888
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-08-23 16:08:09 +0200

    fasdfsdaf

--- create.js
+++ create.js
@@ -6,5 +6,5 @@ createButton.addEventListener("click", create)
 
 function create() {
     out = stringToBinary(urlInput.value)
-    output = "thisisaveryshortdomain.asso.eu.org?url=" + out
+    output.value = "thisisaveryshortdomain.asso.eu.org?url=" + out
 }
