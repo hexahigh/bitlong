dd820a1c1c262a70668f52ef981e5d2721ad834a
Boof <97455552+hexahigh@users.noreply.github.com>
2023-08-24 13:13:27 +0200

    yaff

--- create.js
+++ create.js
@@ -6,25 +6,23 @@ const method = document.getElementById("method")
 createButton.addEventListener("click", create)
 
 function create() {
-    var out, type
+    var out
+    var type
+    type = method.value
 
-    out, type = createMethod()
+    out = createMethod()
     output.value = "https://thisisaveryshortdomain.asso.eu.org?type=" + type + "&url=" + out
 }
 
 function createMethod() {
     var out2
-    var type2
     if (method.value == "binary") {
         out2 = stringToBinary(urlInput.value)
-        type2 = "binary"
     } else if (method.value == "b64") {
         out2 = btoa(urlInput.value)
-        type2 = "binary"
     }
     else if (method.value == "b64_binary") {
         out2 = btoa(stringToBinary(urlInput.value))
-        type2 = "b64_binary"
     }
-    return out2, type2
+    return out2
 }
